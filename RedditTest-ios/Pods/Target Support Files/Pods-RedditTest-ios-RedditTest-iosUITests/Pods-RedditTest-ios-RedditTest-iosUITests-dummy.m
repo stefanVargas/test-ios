ba3c89a215d2dfeaf6b1fd411fbb2d2f5f99d835
stefanv.demoraes <stefanvargas3@gmail.com>
2021-09-23 15:09:03 -0300

    insere cocoapods

---
+++ RedditTest-ios/Pods/Target Support Files/Pods-RedditTest-ios-RedditTest-iosUITests/Pods-RedditTest-ios-RedditTest-iosUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RedditTest_ios_RedditTest_iosUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RedditTest_ios_RedditTest_iosUITests
+@end
